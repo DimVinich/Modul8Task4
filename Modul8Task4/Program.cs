742eaf1393d2064be7ca38aba53b448a6b0a206b
DimVinich <DmitrijVS@gmail.com>
2022-06-22 19:53:41 +0300

    Добавьте файлы проекта.

---
+++ Modul8Task4/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace FinalTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nFileGroup;
+
+            // запросить файл с валидацией
+            string file = @"D:\Temp\Students.dat";
+            Console.WriteLine("Введите имя файла со списком студентов :");
+            //string file = Console.ReadLine();
+
+            if (!File.Exists(file))
+            {
+                Console.WriteLine(" Указанный Вами файл не существует. Программа прекращает свою работу.");
+                Console.ReadKey();
+                return;
+            }
+
+            // Пересоздаём папку на раб.столе
+            string desctopDir = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "/Students/";
+            if (Directory.Exists(desctopDir))
+            {
+                Directory.Delete(desctopDir, true);
+            }
+            Directory.CreateDirectory(desctopDir);
+
+            //  десcерилизовать его файл
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (var fs = new FileStream(file, FileMode.Open))
+            {
+                    Student[] arrStudent = (Student[])formatter.Deserialize(fs);
+
+                // Перебор списка в цикле
+                foreach (Student student in arrStudent)
+                {
+
+                    //  - проверить что такой файл есть  - нетю - генерить новый файл с имененм группы - есть - открыть его
+                    nFileGroup = desctopDir + student.Group + ".txt";
+
+                    FileInfo fileGroup = new FileInfo(nFileGroup);
+                    if (fileGroup.Exists)
+                    {
+                        using (StringWriter sw = fileGroup.AppendText())
+                        {
+                            sw.WriteLine($"Имя: {student.Name} --- Дата рождения: {student.DateOfBirth}  -------- {student.Group}");
+                        }
+                    }
+                    else
+                    {
+
+                    }
+
+
+                    //  записать туда построчно имя, дата рождения
+                    Console.WriteLine($"Имя: {student.Name} --- Дата рождения: {student.DateOfBirth}  -------- {student.Group}");
+                }
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Modul8Task4/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace FinalTask
+{
+    [Serializable]
+    class Student
+    {
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public Student(string aName, string aGroup, DateTime aDateOfBirth)
+        {
+            Name = aName;
+            Group = aGroup;
+            DateOfBirth = aDateOfBirth;
+        }
+
+    }
+}
